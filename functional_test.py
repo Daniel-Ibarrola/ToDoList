2f583d2c84e7290a91aadcbb062048a1e9a21f3a
Daniel-Ibarrola <dis96051@gmail.com>
2022-05-30 19:34:17 -0500

    Added a new item to table in functional test. Test still fails

--- functional_test.py
+++ functional_test.py
@@ -6,40 +6,42 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-	
-	def tearDown(self):
-		self.browser.quit()
-	
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		
-		self.browser.get("http://localhost:8000")
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+    def tearDown(self):
+        self.browser.quit()
 
-		input_box = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			input_box.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
 
-		input_box.send_keys('Buy peacock feathers')
-		input_box.send_keys(Keys.ENTER)
-		time.sleep(1)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-		self.fail('Finish the test!')
-		
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
+
+        self.fail('Finish the test!')
+
 
 if __name__ == "__main__":
-	unittest.main()
-
+    unittest.main()
