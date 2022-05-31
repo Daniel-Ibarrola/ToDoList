c5c48215adfc328bc8c8eec1d91640a50264b81a
Daniel-Ibarrola <dis96051@gmail.com>
2022-05-30 21:23:52 -0500

    Model for list Items and associated migration

--- functional_test.py
+++ functional_test.py
@@ -6,42 +6,45 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get("http://localhost:8000")
 
-        input_box = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            input_box.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-        input_box.send_keys('Buy peacock feathers')
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			input_box.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('Use peacock feathers to make a fly')
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+		input_box.send_keys('Buy peacock feathers')
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+		input_box = self.browser.find_element_by_id('id_new_item')
+		input_box.send_keys('Use peacock feathers to make a fly')
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
 
-        self.fail('Finish the test!')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		self.fail('Finish the test!')
 
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
