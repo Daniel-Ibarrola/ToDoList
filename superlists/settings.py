406357953d9498b64331f1c384a76b6828e7ac67
Daniel-Ibarrola <dis96051@gmail.com>
2022-06-21 18:34:16 -0500

    allowed access to everyone in ALLOWED_HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#q#)tyf5il2!sj&j^q8dc1@kowr4n(*%9o!!i59x3(d8q##(@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
